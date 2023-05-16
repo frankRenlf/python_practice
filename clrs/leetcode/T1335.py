9f1ac4bb6f278aa7b39fd7a34496d3219868eed8
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-05-16 09:02:40 +0800

    update

--- clrs/leetcode/T1335.py
+++ clrs/leetcode/T1335.py
@@ -14,10 +14,12 @@ from typing import List
 
 class Solution:
     def minDifficulty(self, jobDifficulty: List[int], d: int) -> int:
-        if (n := len(jobDifficulty)) < d: return -1
-
-        def dfs(k, d_):
-            if d_ == 1: return max(jobDifficulty[k:])
-            return min(max(jobDifficulty[k: i]) + dfs(i, d_ - 1) for i in range(k + 1, n - d_ + 2))
+        if (n := len(jobDifficulty)) < d:
+            return -1
+        
+        def dfs(left, remain):
+            if remain == 1:
+                return max(jobDifficulty[left:])
+            return min(max(jobDifficulty[left:i]) + dfs(i, remain - 1) for i in range(left + 1, n - remain + 2))
 
         return dfs(0, d)
