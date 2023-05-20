b021ee2c2717b151f23e4feca47f7813575b0502
frankRenlf <frank.renlingfeng@gmail.com>
2023-05-20 08:27:55 +0800

    finish

--- clrs/leetcode/T1373.py
+++ clrs/leetcode/T1373.py
@@ -12,7 +12,7 @@ from clrs.leetcode.utils.TreeNode import TreeNode
 
 
 class SubTree:
-    def __int__(self, is_bst, min_val, max_val, sum_val):
+    def __init__(self, is_bst, min_val, max_val, sum_val):
         self.is_bst = is_bst
         self.min_val = min_val
         self.max_val = max_val
@@ -22,11 +22,20 @@ class SubTree:
 class Solution:
     INF = 0x3f3f3f3f
 
-    def maxSumBST(self, root: Optional[TreeNode]) -> int:
+    def __init__(self):
         self.res = 0
+
+    def maxSumBST(self, root: Optional[TreeNode]) -> int:
         self.dfs(root)
         return self.res
 
     def dfs(self, root):
         if root is None:
             return SubTree(True, self.INF, -self.INF, 0)
+        left = self.dfs(root.left)
+        right = self.dfs(root.right)
+        if left.is_bst and right.is_bst and left.max_val < root.val < right.min_val:
+            cur_sum_val = root.val + left.sum_val + right.sum_val
+            self.res = max(self.res, cur_sum_val)
+            return SubTree(True, min(root.val, left.min_val), max(root.val, right.max_val), cur_sum_val)
+        return SubTree(False, 0, 0, 0)
