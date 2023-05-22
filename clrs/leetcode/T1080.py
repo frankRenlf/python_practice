97698faf6d1df5e949fa2296345ed0ac1ffb2c71
frankRenlf <frank.renlingfeng@gmail.com>
2023-05-22 09:19:01 +0800

    daily work

--- clrs/leetcode/T1080.py
+++ clrs/leetcode/T1080.py
@@ -16,9 +16,28 @@ from clrs.leetcode.utils.TreeNode import TreeNode
 class Solution:
     def sufficientSubset(self, root: Optional[TreeNode], limit: int) -> Optional[TreeNode]:
         res = self.dfs(root, 0, limit)
-        return root if res else None
+        return root if res >= limit else None
 
     def dfs(self, node, pre, limit):
+        if node is None:
+            return -float('inf')
+        if node.left is None and node.right is None:
+            return node.val + pre
+        cur = pre + node.val
+        left_sum = self.dfs(node.left, cur, limit)
+        right_sum = self.dfs(node.right, cur, limit)
+        if left_sum < limit:
+            node.left = None
+        if right_sum < limit:
+            node.right = None
+        res = max(left_sum, right_sum)
+        return res
+
+    def sufficientSubset2(self, root: Optional[TreeNode], limit: int) -> Optional[TreeNode]:
+        res = self.dfs(root, 0, limit)
+        return root if res else None
+
+    def dfs2(self, node, pre, limit):
         if node is None:
             return False
         if node.left is None and node.right is None:
