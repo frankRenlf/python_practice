d01fafbaaff05d9e31a161ab02e82cc6feeddc6b
frankRenlf <frank.renlingfeng@gmail.com>
2023-05-25 10:07:38 +0800

    update

---
+++ clrs/leetcode/T2451.py
@@ -0,0 +1,15 @@
+# -*- coding: UTF-8 -*-
+"""
+    @Author : Frank.Ren
+    @Project : python_practice 
+    @Product : PyCharm
+    @createTime : 2023/5/25 10:07 
+    @Email : sc19lr@leeds.ac.uk
+    @github : https://github.com/frankRenlf
+    @Description : easy
+"""
+from typing import List
+
+
+class Solution:
+    def oddString(self, words: List[str]) -> str:
