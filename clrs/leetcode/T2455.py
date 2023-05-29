317e7968801963af0c1ce410facf63f39080a996
frankRenlf <frank.renlingfeng@gmail.com>
2023-05-29 08:49:33 +0800

    update

---
+++ clrs/leetcode/T2455.py
@@ -0,0 +1,16 @@
+# -*- coding: UTF-8 -*-
+"""
+    @Author : Frank.Ren
+    @Project : python_practice 
+    @Product : PyCharm
+    @createTime : 2023/5/29 08:49 
+    @Email : sc19lr@leeds.ac.uk
+    @github : https://github.com/frankRenlf
+    @Description : easy
+"""
+from typing import List
+
+
+class Solution:
+    def averageValue(self, nums: List[int]) -> int:
+       
