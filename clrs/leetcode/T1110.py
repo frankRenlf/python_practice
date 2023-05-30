4cacd98c4ebb296ad7368f8136c4f48c2054653c
frankRenlf <frank.renlingfeng@gmail.com>
2023-05-30 23:20:54 +0800

    dailywork update

--- clrs/leetcode/T1110.py
+++ clrs/leetcode/T1110.py
@@ -6,7 +6,7 @@
     @createTime : 2023/5/30 23:18 
     @Email : sc19lr@leeds.ac.uk
     @github : https://github.com/frankRenlf
-    @Description :
+    @Description : medium
 """
 from typing import Optional, List
 
