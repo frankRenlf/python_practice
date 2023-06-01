cd2a923a4bab2a0b15dd239e6666244145067d1a
frankRenlf <frank.renlingfeng@gmail.com>
2023-06-01 09:15:34 +0800

    dailywork

--- clrs/leetcode/T2517.py
+++ clrs/leetcode/T2517.py
@@ -8,10 +8,27 @@
     @github : https://github.com/frankRenlf
     @Description : medium
 """
+from math import inf
 from typing import List
 
 
 class Solution:
     def maximumTastiness(self, price: List[int], k: int) -> int:
         price.sort()
-        return price[-k + 1] - price[0]
+        left, right = 0, price[-1] - price[0]
+        while left < right:
+            mid = (left + right + 1) // 2
+            if self.check(price, k, mid):
+                left = mid
+            else:
+                right = mid - 1
+        return left
+
+    def check(self, price: List[int], k: int, tastiness: int) -> bool:
+        prev = -inf
+        cnt = 0
+        for p in price:
+            if p - prev >= tastiness:
+                cnt += 1
+                prev = p
+        return cnt >= k
