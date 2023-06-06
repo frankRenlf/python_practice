6a6c186dc0f132ab74307cfefc87c01736e88283
frankRenlf <frank.renlingfeng@gmail.com>
2023-06-06 10:06:09 +0800

    modify

--- clrs/leetcode/T2352.py
+++ clrs/leetcode/T2352.py
@@ -16,7 +16,6 @@ class Solution:
     def equalPairs(self, grid: List[List[int]]) -> int:
         res, n = 0, len(grid)
         cnt = Counter(tuple(row) for row in grid)
-        res = 0
         for j in range(n):
             res += cnt[tuple([grid[i][j] for i in range(n)])]
         return res
