078d3e9e8b0587da49cc54814f2b9b2305b39725
frankRenlf <frank.renlingfeng@gmail.com>
2023-06-07 08:40:01 +0800

    dailywork

--- clrs/leetcode/T2611.py
+++ clrs/leetcode/T2611.py
@@ -6,7 +6,7 @@
     @createTime : 2023/6/7 08:33 
     @Email : sc19lr@leeds.ac.uk
     @github : https://github.com/frankRenlf
-    @Description :
+    @Description : medium
 """
 from typing import List
 
