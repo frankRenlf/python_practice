c7da4c1bcd7bb05e44cf73857d58396c9b1b82d6
frankRenlf <frank.renlingfeng@gmail.com>
2023-06-08 23:12:16 +0800

    update

--- clrs/main.py
+++ clrs/main.py
@@ -15,12 +15,14 @@ def test(arr):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     sol = Solution()
-    arr1 = [[3, 1, 2, 2], [1, 4, 4, 5], [2, 4, 2, 2], [2, 4, 2, 2]]
-    ret = sol.equalPairs(arr1)
-    print(ret)
+    arr1 = np.array([[3, 1, 2, 2], [1, 4, 4, 5], [2, 4, 2, 2], [2, 4, 2, 2]])
     arr = [1, 2]
     ret = np.linalg.norm(arr)
-    print(arr1[1:])
+    randidx = np.random.permutation(arr1.shape[0])
+    print(randidx)
+    # Take the first K examples as centroids
+    centroids = arr1[randidx[:2]]
+    print(centroids)
     # st = Sorts()
     # arr = [7, 2, 1, 3, 11, 2, 10, 4, 5, 19]
     # st.quick(arr)
