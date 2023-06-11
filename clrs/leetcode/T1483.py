ac5a53c4db1a31b998616c5b756e3425d8bc083f
frankRenlf <frank.renlingfeng@gmail.com>
2023-06-12 00:06:03 +0800

    update

---
+++ clrs/leetcode/T1483.py
@@ -0,0 +1,32 @@
+# -*- coding: UTF-8 -*-
+"""
+    @Author : Frank.Ren
+    @Project : python_practice 
+    @Product : PyCharm
+    @createTime : 2023/6/12 00:05 
+    @Email : sc19lr@leeds.ac.uk
+    @github : https://github.com/frankRenlf
+    @Description : hard
+"""
+from typing import List
+
+
+class TreeAncestor:
+
+    def __init__(self, n: int, parent: List[int]):
+        self.log = 16
+        self.ancestors = [[-1] * self.log for _ in range(n)]
+        for i in range(n):
+            self.ancestors[i][0] = parent[i]
+        for j in range(1, self.log):
+            for i in range(n):
+                if self.ancestors[i][j - 1] != -1:
+                    self.ancestors[i][j] = self.ancestors[self.ancestors[i][j - 1]][j - 1]
+
+    def getKthAncestor(self, node: int, k: int) -> int:
+        for j in range(self.log):
+            if (k >> j) & 1:
+                node = self.ancestors[node][j]
+                if node == -1:
+                    return -1
+        return node
