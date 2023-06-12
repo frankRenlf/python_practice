ec3a886d23d849612da366a4b7ad2b0e2ce72c11
frankRenlf <frank.renlingfeng@gmail.com>
2023-06-13 02:01:42 +0800

    update

--- clrs/leetcode/T2475.py
+++ clrs/leetcode/T2475.py
@@ -14,12 +14,14 @@ from collections import Counter
 
 class Solution:
     def unequalTriplets(self, nums: List[int]) -> int:
-        cnt = Counter(nums)
-        ret = 1
-        for el, val in cnt.items():
-            print(el, val)
-            ret *= val
-        return ret if len(cnt) not in [0, 1, 2] else 1
+        count = Counter(nums)
+        res = 0
+        n = len(nums)
+        t = 0
+        for _, v in count.items():
+            res += t * v * (n - t - v)
+            t += v
+        return res
 
 
 if __name__ == '__main__':
