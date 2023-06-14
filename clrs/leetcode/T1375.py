233e973d349d02c1323804e9ccc3ac83457f3127
frankRenlf <frank.renlingfeng@gmail.com>
2023-06-14 09:19:22 +0800

    update

---
+++ clrs/leetcode/T1375.py
@@ -0,0 +1,21 @@
+# -*- coding: UTF-8 -*-
+"""
+    @Author : Frank.Ren
+    @Project : python_practice 
+    @Product : PyCharm
+    @createTime : 2023/6/14 09:18 
+    @Email : sc19lr@leeds.ac.uk
+    @github : https://github.com/frankRenlf
+    @Description : easy
+"""
+from typing import List
+
+
+class Solution:
+    def numTimesAllBlue(self, flips: List[int]) -> int:
+        ans = right = 0
+        for i, flip in enumerate(flips):
+            right = max(right, flips[i])
+            if right == i + 1:
+                ans += 1
+        return ans
