c10ece2cf5d5ca80ad1d7de0c8d2edee6d04be77
frankRenlf <frank.renlingfeng@gmail.com>
2023-06-18 09:20:10 +0800

    update

---
+++ clrs/leetcode/T1254.py
@@ -0,0 +1,15 @@
+# -*- coding: UTF-8 -*-
+"""
+    @Author : Frank.Ren
+    @Project : python_practice 
+    @Product : PyCharm
+    @createTime : 2023/6/18 09:19 
+    @Email : sc19lr@leeds.ac.uk
+    @github : https://github.com/frankRenlf
+    @Description : medium
+"""
+from typing import List
+
+
+class Solution:
+    def closedIsland(self, grid: List[List[int]]) -> int:
