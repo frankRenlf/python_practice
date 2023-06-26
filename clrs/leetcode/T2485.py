4f35bae98ea8aff60e4fd615961528ccd5024043
frankRenlf <frank.renlingfeng@gmail.com>
2023-06-26 08:46:14 +0800

    update

---
+++ clrs/leetcode/T2485.py
@@ -0,0 +1,24 @@
+# -*- coding: UTF-8 -*-
+"""
+    @Author : Frank.Ren
+    @Project : python_practice 
+    @Product : PyCharm
+    @createTime : 2023/6/26 08:38 
+    @Email : sc19lr@leeds.ac.uk
+    @github : https://github.com/frankRenlf
+    @Description : 
+"""
+
+
+class Solution:
+    def pivotInteger(self, n: int) -> int:
+        left = 0
+        right = sum(i for i in range(n + 1))
+        pos = -1
+        for i in range(1, n + 1):
+            left += i
+            right -= i - 1
+            if left >= right:
+                pos = i
+                break
+        return pos if left == right else -1
