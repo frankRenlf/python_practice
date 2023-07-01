597c2cec45c75457efd8b7ab304d924e66867269
frankRenlf <frank.renlingfeng@gmail.com>
2023-07-01 19:47:00 +0800

    update

---
+++ clrs/leetcode/T1.py
@@ -0,0 +1,10 @@
+# -*- coding: UTF-8 -*-
+"""
+    @Author : Frank.Ren
+    @Project : python_practice 
+    @Product : PyCharm
+    @createTime : 2023/7/1 19:46 
+    @Email : sc19lr@leeds.ac.uk
+    @github : https://github.com/frankRenlf
+    @Description : easy
+"""
