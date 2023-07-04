f0a18adf40c7a3e3b90c44140ef970bae26db5ba
frankRenlf <frank.renlingfeng@gmail.com>
2023-07-04 09:55:48 +0800

    update

---
+++ clrs/leetcode/T2679.py
@@ -0,0 +1,24 @@
+# -*- coding: UTF-8 -*-
+"""
+    @Author : Frank.Ren
+    @Project : python_practice 
+    @Product : PyCharm
+    @createTime : 2023/7/4 09:55 
+    @Email : sc19lr@leeds.ac.uk
+    @github : https://github.com/frankRenlf
+    @Description : medium
+"""
+
+class Solution:
+    def matrixSum(self, nums: List[List[int]]) -> int:
+        res = 0
+        m = len(nums)
+        n = len(nums[0])
+        for i in range(m):
+            nums[i].sort()
+        for j in range(n):
+            max_val = 0
+            for i in range(m):
+                max_val = max(max_val, nums[i][j])
+            res += max_val
+        return res
