d1eaedffd3d590ca5555cbe9f3f84862c32010ff
frankRenlf <frank.renlingfeng@gmail.com>
2023-07-05 14:54:44 +0800

    update

--- clrs/leetcode/T2600.py
+++ clrs/leetcode/T2600.py
@@ -8,3 +8,13 @@
     @github : https://github.com/frankRenlf
     @Description : 
 """
+
+
+class Solution:
+    def kItemsWithMaximumSum(self, numOnes: int, numZeros: int, numNegOnes: int, k: int) -> int:
+        if k <= numOnes:
+            return k
+        elif k <= numOnes + numZeros:
+            return numOnes
+        else:
+            return numOnes - (k - numOnes - numZeros)
