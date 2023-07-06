78abb9e24a475eb4e507dba241b45460dc7a012d
frankRenlf <frank.renlingfeng@gmail.com>
2023-07-06 15:23:24 +0800

    update

---
+++ clrs/leetcode/T2178.py
@@ -0,0 +1,24 @@
+# -*- coding: UTF-8 -*-
+"""
+    @Author : Frank.Ren
+    @Project : python_practice 
+    @Product : PyCharm
+    @createTime : 2023/7/6 15:22 
+    @Email : sc19lr@leeds.ac.uk
+    @github : https://github.com/frankRenlf
+    @Description : 
+"""
+
+
+class Solution:
+    def maximumEvenSplit(self, finalSum: int) -> List[int]:
+        res = []
+        if finalSum % 2 > 0:
+            return res
+        i = 2
+        while i <= finalSum:
+            res.append(i)
+            finalSum -= i
+            i += 2
+        res[-1] += finalSum
+        return res
