3b4150e6a038fbfaa6c23fca7667501894378810
frankRenlf <frank.renlingfeng@gmail.com>
2023-07-08 10:37:16 +0800

    update

--- clrs/leetcode/T167.py
+++ clrs/leetcode/T167.py
@@ -20,3 +20,18 @@ class Solution:
             if target - v in list_dict:
                 return [k + 1, list_dict[target - v] + 1]
         return [-1, -1]
+
+    def twoSum2(self, numbers: List[int], target: int) -> List[int]:
+        n = len(numbers)
+        res = [0, n - 1]
+        while res[0] < res[1]:
+            sum_val = numbers[res[0]] + numbers[res[1]]
+            if sum_val < target:
+                res[0] += 1
+            elif sum_val == target:
+                res[0] += 1
+                res[1] += 1
+                return res
+            else:
+                res[1] -= 1
+        return res
