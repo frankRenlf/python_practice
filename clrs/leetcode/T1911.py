8aff03d7a9b76b4b76183d7b8b99cbc71ced405e
frankRenlf <frank.renlingfeng@gmail.com>
2023-07-11 09:59:09 +0800

    update

--- clrs/leetcode/T1911.py
+++ clrs/leetcode/T1911.py
@@ -6,7 +6,7 @@
     @createTime : 2023/7/11 09:58 
     @Email : sc19lr@leeds.ac.uk
     @github : https://github.com/frankRenlf
-    @Description : 
+    @Description : medium
 """
 from typing import List
 
