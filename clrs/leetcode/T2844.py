4a2adb7c06ce25352cdff672274ad661c848dfa5
frankRenlf <frank.renlingfeng@gmail.com>
2023-07-12 10:23:12 +0800

    update

---
+++ clrs/leetcode/T2844.py
@@ -0,0 +1,14 @@
+# -*- coding: UTF-8 -*-
+"""
+    @Author : Frank.Ren
+    @Project : python_practice 
+    @Product : PyCharm
+    @createTime : 2023/7/12 10:22 
+    @Email : sc19lr@leeds.ac.uk
+    @github : https://github.com/frankRenlf
+    @Description : easy
+"""
+
+
+class Solution:
+    def alternateDigitSum(self, n: int) -> int:
