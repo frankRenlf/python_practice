7bb190297b44fe5230969d2ee8df4d9879b93e79
frankRenlf <frank.renlingfeng@gmail.com>
2023-07-20 23:27:36 +0800

    update

--- clrs/leetcode/T874.py
+++ clrs/leetcode/T874.py
@@ -15,7 +15,7 @@ class Solution:
     def robotSim(self, commands: List[int], obstacles: List[List[int]]) -> int:
         dirs = [[-1, 0], [0, 1], [1, 0], [0, -1]]
         px, py, d = 0, 0, 1
-        mp = set([tuple(i) for i in obstacles])
+        obs_map = set([tuple(i) for i in obstacles])
         res = 0
         for c in commands:
             if c < 0:
@@ -23,7 +23,7 @@ class Solution:
                 d %= 4
             else:
                 for i in range(c):
-                    if tuple([px + dirs[d][0], py + dirs[d][1]]) in mp:
+                    if tuple([px + dirs[d][0], py + dirs[d][1]]) in obs_map:
                         break
                     px, py = px + dirs[d][0], py + dirs[d][1]
                     res = max(res, px * px + py * py)
