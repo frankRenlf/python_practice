36080af15be51e3861dd8c711a5936096f5fffe6
frankRenlf <frank.renlingfeng@gmail.com>
2023-07-21 09:22:29 +0800

    update

---
+++ clrs/leetcode/T1499.py
@@ -0,0 +1,28 @@
+# -*- coding: UTF-8 -*-
+"""
+    @Author : Frank.Ren
+    @Project : python_practice 
+    @Product : PyCharm
+    @createTime : 2023/7/21 09:21 
+    @Email : sc19lr@leeds.ac.uk
+    @github : https://github.com/frankRenlf
+    @Description : 
+"""
+import sys
+from collections import deque
+from typing import List
+
+
+class Solution:
+    def findMaxValueOfEquation(self, points: List[List[int]], k: int) -> int:
+        res = -sys.maxsize
+        q = deque()
+        for x, y in points:
+            while q and x - q[0][1] > k:
+                q.popleft()
+            if q:
+                res = max(res, x + y + q[0][0])
+            while q and y - x >= q[-1][0]:
+                q.pop()
+            q.append([y - x, x])
+        return res
