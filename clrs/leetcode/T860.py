c22ef54a8de4b22371c7e4eb0b24012d213b50d5
frankRenlf <frank.renlingfeng@gmail.com>
2023-07-22 09:57:20 +0800

    update

---
+++ clrs/leetcode/T860.py
@@ -0,0 +1,15 @@
+# -*- coding: UTF-8 -*-
+"""
+    @Author : Frank.Ren
+    @Project : python_practice 
+    @Product : PyCharm
+    @createTime : 2023/7/22 09:54 
+    @Email : sc19lr@leeds.ac.uk
+    @github : https://github.com/frankRenlf
+    @Description : easy
+"""
+from typing import List
+
+
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
