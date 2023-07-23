e2826f3be27927a80fbc02367b3df9027ef53525
frankRenlf <frank.renlingfeng@gmail.com>
2023-07-23 09:30:08 +0800

    update

---
+++ clrs/leetcode/T42.py
@@ -0,0 +1,15 @@
+# -*- coding: UTF-8 -*-
+"""
+    @Author : Frank.Ren
+    @Project : python_practice 
+    @Product : PyCharm
+    @createTime : 2023/7/23 09:29 
+    @Email : sc19lr@leeds.ac.uk
+    @github : https://github.com/frankRenlf
+    @Description : hard
+"""
+from typing import List
+
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
