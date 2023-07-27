d83196eaca2a242e496975b79d1e9acb57e18c68
frankRenlf <frank.renlingfeng@gmail.com>
2023-07-27 10:55:00 +0800

    update

---
+++ clrs/leetcode/T2500.py
@@ -0,0 +1,17 @@
+# -*- coding: UTF-8 -*-
+"""
+    @Author : Frank.Ren
+    @Project : python_practice 
+    @Product : PyCharm
+    @createTime : 2023/7/27 10:54 
+    @Email : sc19lr@leeds.ac.uk
+    @github : https://github.com/frankRenlf
+    @Description : easy
+"""
+from typing import List
+
+
+class Solution:
+    def deleteGreatestValue(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        
