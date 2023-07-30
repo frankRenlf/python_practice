31b81ea3c7fe23a986a4b6e6d91cef77dfeb5c91
frankRenlf <frank.renlingfeng@gmail.com>
2023-07-30 11:08:45 +0800

    update

---
+++ clrs/leetcode/T142.py
@@ -0,0 +1,20 @@
+# -*- coding: UTF-8 -*-
+"""
+    @Author : Frank.Ren
+    @Project : python_practice 
+    @Product : PyCharm
+    @createTime : 2023/7/30 11:08 
+    @Email : sc19lr@leeds.ac.uk
+    @github : https://github.com/frankRenlf
+    @Description : medium
+"""
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
