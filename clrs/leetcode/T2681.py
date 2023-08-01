663df0efecc5c679543e5ecbdbe11aa5bbbfda2b
frankRenlf <frank.renlingfeng@gmail.com>
2023-08-01 17:07:17 +0800

    update

--- clrs/leetcode/T2681.py
+++ clrs/leetcode/T2681.py
@@ -8,5 +8,17 @@
     @github : https://github.com/frankRenlf
     @Description : hard
 """
+from typing import List
 
 
+class Solution:
+    def sumOfPower(self, nums: List[int]) -> int:
+        nums.sort()
+        dp = [0 for i in range(len(nums))]
+        pre_sum = [0 for i in range(len(nums) + 1)]
+        res, mod = 0, 10 ** 9 + 7
+        for i in range(len(nums)):
+            dp[i] = (nums[i] + pre_sum[i]) % mod
+            pre_sum[i + 1] = (pre_sum[i] + nums[i] + pre_sum[i]) % mod
+            res = (res + nums[i] * nums[i] * (nums[i] + pre_sum[i]) % mod) % mod
+        return res
