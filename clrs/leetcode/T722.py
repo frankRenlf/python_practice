df2269a70e78389a810c546b16bcf1d219b883f1
frankRenlf <frank.renlingfeng@gmail.com>
2023-08-03 10:43:58 +0800

    update

---
+++ clrs/leetcode/T722.py
@@ -0,0 +1,15 @@
+# -*- coding: UTF-8 -*-
+"""
+    @Author : Frank.Ren
+    @Project : python_practice 
+    @Product : PyCharm
+    @createTime : 2023/8/3 10:43 
+    @Email : sc19lr@leeds.ac.uk
+    @github : https://github.com/frankRenlf
+    @Description : medium
+"""
+from typing import List
+
+
+class Solution:
+    def removeComments(self, source: List[str]) -> List[str]:
