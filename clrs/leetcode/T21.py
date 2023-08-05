d71bc7bd33aea0fc27b3fbc7500c805abebed0b5
frankRenlf <frank.renlingfeng@gmail.com>
2023-08-05 10:47:35 +0800

    update

--- clrs/leetcode/T21.py
+++ clrs/leetcode/T21.py
@@ -36,3 +36,13 @@ class Solution:
         prev.next = l1 if l1 is not None else l2
 
         return prehead.next
+
+    def mergeTwoLists_dfs(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if not l1: return l2  # 终止条件，直到两个链表都空
+        if not l2: return l1
+        if l1.val <= l2.val:  # 递归调用
+            l1.next = self.mergeTwoLists_dfs(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists_dfs(l1, l2.next)
+            return l2
