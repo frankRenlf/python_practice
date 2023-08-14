5a32d1f16c7c6ec0f2ac995f9be3476052c8fa5f
frankRenlf <frank.renlingfeng@gmail.com>
2023-08-14 10:20:53 +0800

    update

--- clrs/leetcode/T88.py
+++ clrs/leetcode/T88.py
@@ -13,7 +13,7 @@ from typing import List
 
 
 class Solution:
-    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+    def merge_forward(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
         """
@@ -32,8 +32,29 @@ class Solution:
                 j += 1
             nums1[k] = add
 
+    def merge_backward(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        p1, p2 = m - 1, n - 1
+        tail = m + n - 1
+        while p1 >= 0 or p2 >= 0:
+            if p1 == -1:
+                nums1[tail] = nums2[p2]
+                p2 -= 1
+            elif p2 == -1:
+                nums1[tail] = nums1[p1]
+                p1 -= 1
+            elif nums1[p1] > nums2[p2]:
+                nums1[tail] = nums1[p1]
+                p1 -= 1
+            else:
+                nums1[tail] = nums2[p2]
+                p2 -= 1
+            tail -= 1
+
 
 if __name__ == "__main__":
     sol = Solution()
     grid = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-    print(sol.merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3))
+    print(sol.merge_backward([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3))
