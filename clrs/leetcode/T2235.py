e424b0287c979fda30fa4c1c64945d574cf7693a
frankRenlf <frank.renlingfeng@gmail.com>
2023-08-19 10:23:24 +0800

    Update

---
+++ clrs/leetcode/T2235.py
@@ -0,0 +1,18 @@
+# -*- coding: UTF-8 -*-
+"""
+    @Author : Frank.Ren
+    @Project : python_practice 
+    @Product : PyCharm
+    @createTime : 2023/8/19 10:23 
+    @Email : sc19lr@leeds.ac.uk
+    @github : https://github.com/frankRenlf
+    @Description : easy
+"""
+
+
+# if __name__ == "__main__":
+
+
+class Solution:
+    def sum(self, num1: int, num2: int) -> int:
+        return num1 + num2
