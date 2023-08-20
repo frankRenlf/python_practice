e9e126fb75ef4d53f9c7507a5d0e7699874eb766
frankRenlf <frank.renlingfeng@gmail.com>
2023-08-21 00:19:37 +0800

    update

---
+++ clrs/leetcode/T2337.py
@@ -0,0 +1,39 @@
+# -*- coding: UTF-8 -*-
+"""
+    @Author : Frank.Ren
+    @Project : python_practice 
+    @Product : PyCharm
+    @createTime : 2023/8/21 00:13 
+    @Email : sc19lr@leeds.ac.uk
+    @github : https://github.com/frankRenlf
+    @Description : medium
+"""
+
+
+# if __name__ == "__main__":
+class Solution:
+    def canChange(self, start: str, target: str) -> bool:
+        n = len(start)
+        i = j = 0
+        while i < n and j < n:
+            while i < n and start[i] == '_':
+                i += 1
+            while j < n and target[j] == '_':
+                j += 1
+            if i < n and j < n:
+                if start[i] != target[j]:
+                    return False
+                c = start[i]
+                if c == 'L' and i < j or c == 'R' and i > j:
+                    return False
+                i += 1
+                j += 1
+        while i < n:
+            if start[i] != '_':
+                return False
+            i += 1
+        while j < n:
+            if target[j] != '_':
+                return False
+            j += 1
+        return True
