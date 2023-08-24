e5974efa1242a07dec63f3c651956f32219300ed
frankRenlf <frank.renlingfeng@gmail.com>
2023-08-24 10:41:05 +0800

    update

---
+++ clrs/leetcode/T1267.py
@@ -0,0 +1,13 @@
+# -*- coding: UTF-8 -*-
+"""
+    @Author : Frank.Ren
+    @Project : python_practice 
+    @Product : PyCharm
+    @createTime : 2023/8/24 10:40 
+    @Email : sc19lr@leeds.ac.uk
+    @github : https://github.com/frankRenlf
+    @Description : medium
+"""
+class Solution:
+    def countServers(self, grid: List[List[int]]) -> int:
+# if __name__ == "__main__":
