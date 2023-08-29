fb99ef844e54ad60abbc0c994e1698da706cbac3
frankRenlf <frank.renlingfeng@gmail.com>
2023-08-29 12:36:26 +0800

    update

--- clrs/leetcode/T823.py
+++ clrs/leetcode/T823.py
@@ -13,4 +13,22 @@ from typing import List
 
 class Solution:
     def numFactoredBinaryTrees(self, arr: List[int]) -> int:
+        n = len(arr)
+        arr = sorted(arr)
+        dp = [1] * n
+        res, mod = 0, 10 ** 9 + 7
+        for i in range(n):
+            left, right = 0, i - 1
+            while left <= right:
+                while right >= left and arr[left] * arr[right] > arr[i]:
+                    right -= 1
+                if right >= left and arr[left] * arr[right] == arr[i]:
+                    if right != left:
+                        dp[i] = (dp[i] + dp[left] * dp[right] * 2) % mod
+                    else:
+                        dp[i] = (dp[i] + dp[left] * dp[right]) % mod
+                left += 1
+            res = (res + dp[i]) % mod
+        return res
+
 # if __name__ == "__main__":
