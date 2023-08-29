be0ef46cce738b1fd65996aa13335608e1e956b7
frankRenlf <frank.renlingfeng@gmail.com>
2023-08-29 12:19:58 +0800

    update

---
+++ clrs/leetcode/T823.py
@@ -0,0 +1,16 @@
+# -*- coding: UTF-8 -*-
+"""
+    @Author : Frank.Ren
+    @Project : python_practice 
+    @Product : PyCharm
+    @createTime : 2023/8/29 12:19 
+    @Email : sc19lr@leeds.ac.uk
+    @github : https://github.com/frankRenlf
+    @Description : medium
+"""
+from typing import List
+
+
+class Solution:
+    def numFactoredBinaryTrees(self, arr: List[int]) -> int:
+# if __name__ == "__main__":
