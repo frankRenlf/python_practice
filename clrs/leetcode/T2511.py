e20b5e650efebd9785a0c8e59b257f7166c4edc7
frankRenlf <frank.renlingfeng@gmail.com>
2023-09-02 00:35:54 +0800

    update

---
+++ clrs/leetcode/T2511.py
@@ -0,0 +1,17 @@
+# -*- coding: UTF-8 -*-
+"""
+    @Author : Frank.Ren
+    @Project : python_practice 
+    @Product : PyCharm
+    @createTime : 2023/9/2 00:35 
+    @Email : sc19lr@leeds.ac.uk
+    @github : https://github.com/frankRenlf
+    @Description : easy
+"""
+from typing import List
+
+
+class Solution:
+    def captureForts(self, forts: List[int]) -> int:
+
+# if __name__ == "__main__":
