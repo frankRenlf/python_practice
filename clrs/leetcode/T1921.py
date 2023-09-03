75cf5ecfef0e01619124a38e46add88218c02186
frankRenlf <frank.renlingfeng@gmail.com>
2023-09-03 08:24:35 +0800

    update

---
+++ clrs/leetcode/T1921.py
@@ -0,0 +1,17 @@
+# -*- coding: UTF-8 -*-
+"""
+    @Author : Frank.Ren
+    @Project : python_practice 
+    @Product : PyCharm
+    @createTime : 2023/9/3 08:23 
+    @Email : sc19lr@leeds.ac.uk
+    @github : https://github.com/frankRenlf
+    @Description : medium
+"""
+from typing import List
+
+
+class Solution:
+    def eliminateMaximum(self, dist: List[int], speed: List[int]) -> int:
+        
+# if __name__ == "__main__":
