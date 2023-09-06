d68cbcafb359ddae76bbc0fd95fee9b792494763
frankRenlf <frank.renlingfeng@gmail.com>
2023-09-06 09:25:35 +0800

    update

---
+++ clrs/leetcode/T1123.py
@@ -0,0 +1,39 @@
+# -*- coding: UTF-8 -*-
+"""
+    @Author : Frank.Ren
+    @Project : python_practice 
+    @Product : PyCharm
+    @createTime : 2023/9/6 09:23 
+    @Email : sc19lr@leeds.ac.uk
+    @github : https://github.com/frankRenlf
+    @Description : medium
+"""
+from typing import Optional
+
+from clrs.leetcode.utils.TreeNode import TreeNode
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def lcaDeepestLeaves(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        def f(root):
+            if not root:
+                return 0, None
+
+            d1, lca1 = f(root.left)
+            d2, lca2 = f(root.right)
+
+            if d1 > d2:
+                return d1 + 1, lca1
+            if d1 < d2:
+                return d2 + 1, lca2
+            return d1 + 1, root
+
+        return f(root)[1]
+
+# if __name__ == "__main__":
