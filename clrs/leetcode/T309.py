6663b657804eba2b6c1f3e2dfedaf3dc10b29137
frankRenlf <frank.renlingfeng@gmail.com>
2023-10-05 09:34:21 +0800

    update

---
+++ clrs/leetcode/T309.py
@@ -0,0 +1,30 @@
+# -*- coding: UTF-8 -*-
+"""
+    @Author : Frank.Ren
+    @Project : python_practice 
+    @Product : PyCharm
+    @createTime : 2023/10/5 09:20 
+    @Email : sc19lr@leeds.ac.uk
+    @github : https://github.com/frankRenlf
+    @Description : medium
+"""
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        n = len(prices)
+        dp = [[0] * 3 for _ in range(n)]
+        dp[0][0] = -prices[0]
+        dp[0][1] = 0
+        dp[0][2] = 0
+        for i in range(1, n):
+            dp[i][0] = max(dp[i - 1][0], dp[i - 1][2] - prices[i])
+            dp[i][1] = dp[i - 1][0] + prices[i]
+            dp[i][2] = max(dp[i - 1][1], dp[i - 1][2])
+        return max(dp[n - 1][2], dp[n - 1][1])
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.maxProfit([1, 2, 3, 0, 2]))
