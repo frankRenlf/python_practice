9618aae0e9bdba3cc8d79aa356875d613e1d5e2b
frankRenlf <frank.renlingfeng@gmail.com>
2023-10-06 10:15:01 +0800

    update

---
+++ clrs/leetcode/T714.py
@@ -0,0 +1,28 @@
+# -*- coding: UTF-8 -*-
+"""
+    @Author : Frank.Ren
+    @Project : python_practice 
+    @Product : PyCharm
+    @createTime : 2023/10/6 09:15 
+    @Email : sc19lr@leeds.ac.uk
+    @github : https://github.com/frankRenlf
+    @Description : medium
+"""
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int], fee: int) -> int:
+        n = len(prices)
+        dp = [[0] * 2 for _ in range(n)]
+        dp[0][0] = -prices[0]
+        dp[0][1] = 0
+        for i in range(1, n):
+            dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] - prices[i])
+            dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] + prices[i] - fee)
+        return max(dp[n - 1])
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.maxProfit(prices=[1, 3, 7, 5, 10, 3], fee=3))
