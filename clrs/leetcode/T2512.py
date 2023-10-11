7051b2f0e7fa34c6bd5cb7d3ad4e14b64a2320d7
frankRenlf <frank.renlingfeng@gmail.com>
2023-10-11 09:27:15 +0800

    update

--- clrs/leetcode/T2512.py
+++ clrs/leetcode/T2512.py
@@ -12,9 +12,37 @@ from typing import List
 
 
 class Solution:
+
+    def topStudents_fast(self, positive_feedback, negative_feedback, report, student_id, k):
+        words = {}
+        for w in positive_feedback:
+            words[w] = 3
+        for w in negative_feedback:
+            words[w] = -1
+        A = []
+        for s, i in zip(report, student_id):
+            score = sum(words.get(w, 0) for w in s.split())
+            A.append([-score, i])
+        A.sort()
+        return [i for v, i in A[:k]]
+
     def topStudents(self,
                     positive_feedback: List[str],
                     negative_feedback: List[str],
                     report: List[str],
                     student_id: List[int], k: int) -> List[int]:
-# if __name__ == "__main__":
+        n = len(student_id)
+        scores = list(map(list, zip(student_id, [0] * n)))
+        for i, sentence in enumerate(report):
+            for word in sentence.split():
+                if word in positive_feedback:
+                    scores[i][1] += 3
+                elif word in negative_feedback:
+                    scores[i][1] -= 1
+        return [item[0] for item in sorted(scores, key=lambda x: (-x[1], x[0]))][:k]
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.topStudents(positive_feedback=["smart", "brilliant", "studious"], negative_feedback=["not"],
+                          report=["this student is not studious", "the student is smart"], student_id=[1, 2], k=2))
