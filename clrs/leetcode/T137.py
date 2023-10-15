c75f1c2723c421e9dfa19427a90da11506bddb07
frankRenlf <frank.renlingfeng@gmail.com>
2023-10-15 09:43:20 +0800

    update

--- clrs/leetcode/T137.py
+++ clrs/leetcode/T137.py
@@ -16,4 +16,13 @@ class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         return Counter(nums).most_common()[-1][0]
 
+
+class Solution_best:
+    def singleNumber(self, nums: List[int]) -> int:
+        a = b = 0
+        for num in nums:
+            b = ~a & (b ^ num)
+            a = ~b & (a ^ num)
+        return b
+
 # if __name__ == "__main__":
