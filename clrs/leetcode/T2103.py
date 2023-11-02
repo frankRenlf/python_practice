4d35ddddf8daba4501928f35d3f6213a427d426a
frankRenlf <frank.renlingfeng@gmail.com>
2023-11-02 10:48:45 +0800

    update

---
+++ clrs/leetcode/T2103.py
@@ -0,0 +1,24 @@
+# -*- coding: UTF-8 -*-
+"""
+    @Author : Frank.Ren
+    @Project : python_practice 
+    @Product : PyCharm
+    @createTime : 2023/11/2 10:48 
+    @Email : e1143935@u.nus.edu
+    @github : https://github.com/frankRenlf
+    @Description : easy
+"""
+
+
+class Solution:
+    def countPoints(self, rings: str) -> int:
+        POLE_NUM = 10
+        mapping = {'R': 0, 'G': 1, 'B': 2}
+        state = [0] * POLE_NUM
+        for i in range(0, len(rings), 2):
+            color = rings[i]
+            pole_index = ord(rings[i + 1]) - ord('0')
+            state[pole_index] |= 1 << mapping[color]
+        return sum(state[i] == 7 for i in range(POLE_NUM))
+
+# if __name__ == "__main__":
