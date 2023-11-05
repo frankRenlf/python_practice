03f1a8045ab51478027c2162fe462f5207693cf4
frankRenlf <frank.renlingfeng@gmail.com>
2023-11-05 10:42:36 +0800

    update

--- clrs/leetcode/T187.py
+++ clrs/leetcode/T187.py
@@ -9,6 +9,7 @@
     @Description : medium
 """
 from collections import defaultdict
+from typing import List
 
 L = 10
 
@@ -22,6 +23,27 @@ class Solution:
             cnt[sub] += 1
             if cnt[sub] == 2:
                 ans.append(sub)
+        return ans
+
+
+bin = {'A': 0, 'C': 1, 'G': 2, 'T': 3}
+
+
+class Solution2:
+    def findRepeatedDnaSequences(self, s: str) -> List[str]:
+        n = len(s)
+        if n <= L:
+            return []
+        ans = []
+        x = 0
+        for ch in s[:L - 1]:
+            x = (x << 2) | bin[ch]
+        cnt = defaultdict(int)
+        for i in range(n - L + 1):
+            x = ((x << 2) | bin[s[i + L - 1]]) & ((1 << (L * 2)) - 1)
+            cnt[x] += 1
+            if cnt[x] == 2:
+                ans.append(s[i: i + L])
         return ans
 
 # if __name__ == "__main__":
