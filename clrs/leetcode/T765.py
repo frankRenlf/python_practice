81e25b566d9c891818c305bf4294de950bd49cc7
frankRenlf <frank.renlingfeng@gmail.com>
2023-11-11 10:22:03 +0800

    update

---
+++ clrs/leetcode/T765.py
@@ -0,0 +1,28 @@
+# -*- coding: UTF-8 -*-
+"""
+    @Author : Frank.Ren
+    @Project : python_practice 
+    @Product : PyCharm
+    @createTime : 2023/11/11 10:21 
+    @Email : e1143935@u.nus.edu
+    @github : https://github.com/frankRenlf
+    @Description : hard
+"""
+from typing import List
+
+
+class Solution:
+    def minSwapsCouples(self, row: List[int]) -> int:
+        def find(x: int) -> int:
+            if p[x] != x:
+                p[x] = find(p[x])
+            return p[x]
+
+        n = len(row) >> 1
+        p = list(range(n))
+        for i in range(0, len(row), 2):
+            a, b = row[i] >> 1, row[i + 1] >> 1
+            p[find(a)] = find(b)
+        return n - sum(i == find(i) for i in range(n))
+
+# if __name__ == "__main__":
---
+++ clrs/leetcode/utils/UnionTemplate.py
@@ -0,0 +1,35 @@
+# -*- coding: UTF-8 -*-
+"""
+    @Author : Frank.Ren
+    @Project : python_practice 
+    @Product : PyCharm
+    @createTime : 2023/11/11 10:21 
+    @Email : e1143935@u.nus.edu
+    @github : https://github.com/frankRenlf
+    @Description : union find template
+"""
+
+
+class UnionFind:
+    def __init__(self, n):
+        self.p = list(range(n))
+        self.size = [1] * n
+
+    def find(self, x):
+        if self.p[x] != x:
+            self.p[x] = self.find(self.p[x])
+        return self.p[x]
+
+    def union(self, a, b):
+        pa, pb = self.find(a), self.find(b)
+        if pa == pb:
+            return False
+        if self.size[pa] > self.size[pb]:
+            self.p[pb] = pa
+            self.size[pa] += self.size[pb]
+        else:
+            self.p[pa] = pb
+            self.size[pb] += self.size[pa]
+        return True
+
+# if __name__ == "__main__":
