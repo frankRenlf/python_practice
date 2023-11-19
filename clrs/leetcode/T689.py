3472ccb937fb879d86b76b8ee4fd947adf8bbb33
frankRenlf <frank.renlingfeng@gmail.com>
2023-11-19 10:05:16 +0800

    update

---
+++ clrs/leetcode/T689.py
@@ -0,0 +1,17 @@
+# -*- coding: UTF-8 -*-
+"""
+    @Author : Frank.Ren
+    @Project : python_practice 
+    @Product : PyCharm
+    @createTime : 2023/11/19 10:00 
+    @Email : e1143935@u.nus.edu
+    @github : https://github.com/frankRenlf
+    @Description : hard
+"""
+from typing import List
+
+
+class Solution:
+    def maxSumOfThreeSubarrays(self, nums: List[int], k: int) -> List[int]:
+        
+# if __name__ == "__main__":
