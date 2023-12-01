6ed6c1fa51111c361f355f82838562e89ab98128
frankRenlf <frank.renlingfeng@gmail.com>
2023-12-01 09:18:11 +0800

    update

---
+++ clrs/leetcode/T2661.py
@@ -0,0 +1,16 @@
+# -*- coding: UTF-8 -*-
+"""
+    @Author : Frank.Ren
+    @Project : python_practice 
+    @Product : PyCharm
+    @createTime : 2023/12/1 09:17 
+    @Email : e1143935@u.nus.edu
+    @github : https://github.com/frankRenlf
+    @Description : medium
+"""
+from typing import List
+
+
+class Solution:
+    def firstCompleteIndex(self, arr: List[int], mat: List[List[int]]) -> int:
+# if __name__ == "__main__":
