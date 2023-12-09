7dd6fcf8c530ae84575f404a90baf2a166106f40
frankRenlf <frank.renlingfeng@gmail.com>
2023-12-09 10:15:58 +0800

    update

---
+++ clrs/leetcode/T2048.py
@@ -0,0 +1,14 @@
+# -*- coding: UTF-8 -*-
+"""
+    @Author : Frank.Ren
+    @Project : python_practice 
+    @Product : PyCharm
+    @createTime : 2023/12/9 10:15 
+    @Email : e1143935@u.nus.edu
+    @github : https://github.com/frankRenlf
+    @Description : medium
+"""
+class Solution:
+    def nextBeautifulNumber(self, n: int) -> int:
+
+# if __name__ == "__main__":
