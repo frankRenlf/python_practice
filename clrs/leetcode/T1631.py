e7d092f72c3dd521771b22dcb6344b9366634703
frankRenlf <frank.renlingfeng@gmail.com>
2023-12-11 09:04:06 +0800

    update

---
+++ clrs/leetcode/T1631.py
@@ -0,0 +1,42 @@
+# -*- coding: UTF-8 -*-
+"""
+    @Author : Frank.Ren
+    @Project : python_practice 
+    @Product : PyCharm
+    @createTime : 2023/12/11 08:41 
+    @Email : e1143935@u.nus.edu
+    @github : https://github.com/frankRenlf
+    @Description : medium
+"""
+import heapq
+import sys
+from typing import List
+
+
+class Solution:
+    def minimumEffortPath(self, heights: List[List[int]]) -> int:
+        m, n = len(heights), len(heights[0])
+        q = [(0, 0, 0)]
+        dist = [0] + [float("inf")] * (m * n - 1)
+        seen = set()
+
+        while q:
+            d, x, y = heapq.heappop(q)
+            iden = x * n + y
+            if iden in seen:
+                continue
+            if (x, y) == (m - 1, n - 1):
+                break
+
+            seen.add(iden)
+            for nx, ny in [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]:
+                if 0 <= nx < m and 0 <= ny < n and max(d, abs(heights[x][y] - heights[nx][ny])) <= dist[nx * n + ny]:
+                    dist[nx * n + ny] = max(d, abs(heights[x][y] - heights[nx][ny]))
+                    heapq.heappush(q, (dist[nx * n + ny], nx, ny))
+
+        return dist[m * n - 1]
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.minimumEffortPath([[1, 2, 2], [3, 8, 2], [5, 3, 5]]))
