67ad0df2fdc3a3674883f5545f07012772cce119
frankRenlf <frank.renlingfeng@gmail.com>
2023-12-15 11:03:46 +0800

    update

--- clrs/leetcode/T2415.py
+++ clrs/leetcode/T2415.py
@@ -8,9 +8,11 @@
     @github : https://github.com/frankRenlf
     @Description : medium
 """
+import queue
 from typing import Optional
 
-from clrs.leetcode.utils.TreeNode import TreeNode
+from clrs.leetcode.utils.TreeNode import TreeNode, create_perfect_binary_tree
+from queue import Queue
 
 
 # Definition for a binary tree node.
@@ -21,5 +23,34 @@ from clrs.leetcode.utils.TreeNode import TreeNode
 #         self.right = right
 class Solution:
     def reverseOddLevels(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        q = Queue()
+        q.put(root)
+        depth = 0
+        while not q.empty():
+            n = q.qsize()
+            arr = []
+            while n > 0:
+                n -= 1
+                node = q.get()
+                if depth % 2 != 0:
+                    arr.append(node)
+                if node.left is not None:
+                    q.put(node.left)
+                    q.put(node.right)
+            if depth % 2 != 0:
+                for i in range(len(arr) // 2):
+                    tmp = arr[i].val
+                    arr[i].val = arr[n - i - 1].val
+                    arr[n - i - 1].val = tmp
+            depth += 1
+        return root
 
-# if __name__ == "__main__":
+
+# 示例
+
+if __name__ == "__main__":
+    arr = [i for i in range(1, 16)]
+    tree = create_perfect_binary_tree(arr)
+    sol = Solution()
+    sol.reverseOddLevels(tree)
+    print(1)
--- clrs/leetcode/utils/TreeNode.py
+++ clrs/leetcode/utils/TreeNode.py
@@ -13,3 +13,30 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
+
+
+def create_perfect_binary_tree(arr):
+    if not arr:
+        return None
+
+    root = TreeNode(arr[0])
+    queue = [root]
+    i = 1
+
+    # 使用队列按层次构建树
+    while i < len(arr):
+        current = queue.pop(0)
+
+        # 添加左子节点
+        if i < len(arr):
+            current.left = TreeNode(arr[i])
+            queue.append(current.left)
+            i += 1
+
+        # 添加右子节点
+        if i < len(arr):
+            current.right = TreeNode(arr[i])
+            queue.append(current.right)
+            i += 1
+
+    return root
