ab8f218f08b27c955db232a6686dc4eca970664f
frankRenlf <frank.renlingfeng@gmail.com>
2023-12-15 10:44:44 +0800

    update

---
+++ clrs/leetcode/T2415.py
@@ -0,0 +1,25 @@
+# -*- coding: UTF-8 -*-
+"""
+    @Author : Frank.Ren
+    @Project : python_practice 
+    @Product : PyCharm
+    @createTime : 2023/12/15 10:44 
+    @Email : e1143935@u.nus.edu
+    @github : https://github.com/frankRenlf
+    @Description : medium
+"""
+from typing import Optional
+
+from clrs.leetcode.utils.TreeNode import TreeNode
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def reverseOddLevels(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+
+# if __name__ == "__main__":
