9b83735c065035fcb4af5b630d0a4eaff7b62003
frankRenlf <frank.renlingfeng@gmail.com>
2023-12-20 09:10:23 +0800

    update

---
+++ clrs/leetcode/T2828.py
@@ -0,0 +1,17 @@
+# -*- coding: UTF-8 -*-
+"""
+    @Author : Frank.Ren
+    @Project : python_practice 
+    @Product : PyCharm
+    @createTime : 2023/12/20 09:10 
+    @Email : e1143935@u.nus.edu
+    @github : https://github.com/frankRenlf
+    @Description : easy
+"""
+from typing import List
+
+
+class Solution:
+    def isAcronym(self, words: List[str], s: str) -> bool:
+
+# if __name__ == "__main__":
