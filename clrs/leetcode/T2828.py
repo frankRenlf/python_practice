bb23487cee19998420710850daae357fe4cd1aad
frankRenlf <frank.renlingfeng@gmail.com>
2023-12-20 09:18:06 +0800

    update

--- clrs/leetcode/T2828.py
+++ clrs/leetcode/T2828.py
@@ -9,9 +9,21 @@
     @Description : easy
 """
 from typing import List
+from collections import Counter
 
 
 class Solution:
     def isAcronym(self, words: List[str], s: str) -> bool:
+        cnt = len(s)
+        if len(words) != cnt:
+            return False
+        for i, word in enumerate(words):
+            if s[i] != word[0]:
+                return False
+        return True
 
-# if __name__ == "__main__":
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.isAcronym(words = ["never","gonna","give","up","on","you"], s = "ngguoy"))
+    print("1234"[0])
