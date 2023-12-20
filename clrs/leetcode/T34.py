b241679b308f4469717e6cd18aea7413743bbf69
frankRenlf <frank.renlingfeng@gmail.com>
2023-12-20 17:38:30 +0800

    update

---
+++ clrs/leetcode/T34.py
@@ -0,0 +1,38 @@
+# -*- coding: UTF-8 -*-
+"""
+    @Author : Frank.Ren
+    @Project : python_practice 
+    @Product : PyCharm
+    @createTime : 2023/12/20 17:19 
+    @Email : e1143935@u.nus.edu
+    @github : https://github.com/frankRenlf
+    @Description : medium
+"""
+from typing import List
+
+
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        n = len(nums)
+        if n == 0:
+            return [-1, -1]
+        left = 0
+        right = n - 1
+        while left < right:
+            mid = (left + right) // 2
+            if nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid
+        if nums[left] != target:
+            return [-1, -1]
+        l = left
+        right = n - 1
+        while left < right:
+            mid = (left + right + 1) // 2
+            if nums[mid] > target:
+                right = mid - 1
+            else:
+                left = mid
+        return [l, right]
+# if __name__ == "__main__":
