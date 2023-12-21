aa99fc3d62379218e19b8db8efa90beb551f97c5
frankRenlf <frank.renlingfeng@gmail.com>
2023-12-21 15:34:30 +0800

    update

---
+++ clrs/leetcode/T153.py
@@ -0,0 +1,28 @@
+# -*- coding: UTF-8 -*-
+"""
+    @Author : Frank.Ren
+    @Project : python_practice 
+    @Product : PyCharm
+    @createTime : 2023/12/21 15:27 
+    @Email : e1143935@u.nus.edu
+    @github : https://github.com/frankRenlf
+    @Description : medium
+"""
+from typing import List
+
+
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        low, high = 0, len(nums) - 1
+        while low < high:
+            pivot = low + (high - low) // 2
+            if nums[pivot] < nums[high]:
+                high = pivot
+            else:
+                low = pivot + 1
+        return nums[low]
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.findMin([3, 4, 5, 1, 2]))
