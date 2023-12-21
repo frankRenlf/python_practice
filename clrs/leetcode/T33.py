08535172834c3d365d004203eac4e0d2ad1e1f16
frankRenlf <frank.renlingfeng@gmail.com>
2023-12-21 11:30:41 +0800

    update

---
+++ clrs/leetcode/T33.py
@@ -0,0 +1,17 @@
+# -*- coding: UTF-8 -*-
+"""
+    @Author : Frank.Ren
+    @Project : python_practice 
+    @Product : PyCharm
+    @createTime : 2023/12/21 11:30 
+    @Email : e1143935@u.nus.edu
+    @github : https://github.com/frankRenlf
+    @Description : medium
+"""
+from typing import List
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+
+# if __name__ == "__main__":
