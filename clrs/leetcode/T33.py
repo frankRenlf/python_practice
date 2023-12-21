881628239e216f6194cb22621ae414a974e2fa13
frankRenlf <frank.renlingfeng@gmail.com>
2023-12-21 11:35:40 +0800

    update

--- clrs/leetcode/T33.py
+++ clrs/leetcode/T33.py
@@ -13,5 +13,32 @@ from typing import List
 
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
+        if not nums:
+            return False
+
+        n = len(nums)
+        if n == 1:
+            return nums[0] == target
+
+        l, r = 0, n - 1
+        while l <= r:
+            mid = (l + r) // 2
+            if nums[mid] == target:
+                return mid
+            if nums[l] == nums[mid] and nums[mid] == nums[r]:
+                l += 1
+                r -= 1
+            elif nums[l] <= nums[mid]:
+                if nums[l] <= target and target < nums[mid]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+            else:
+                if nums[mid] < target and target <= nums[n - 1]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+
+        return -1
 
 # if __name__ == "__main__":
