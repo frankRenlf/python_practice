c0cad2f9dd0ae6a622113369ea3201710f0f67d4
frankRenlf <frank.renlingfeng@gmail.com>
2023-12-27 09:54:51 +0800

    update

---
+++ clrs/leetcode/T2660.py
@@ -0,0 +1,16 @@
+# -*- coding: UTF-8 -*-
+"""
+    @Author : Frank.Ren
+    @Project : python_practice 
+    @Product : PyCharm
+    @createTime : 2023/12/27 09:53 
+    @Email : e1143935@u.nus.edu
+    @github : https://github.com/frankRenlf
+    @Description : easy
+"""
+from typing import List
+
+
+class Solution:
+    def isWinner(self, player1: List[int], player2: List[int]) -> int:
+# if __name__ == "__main__":
