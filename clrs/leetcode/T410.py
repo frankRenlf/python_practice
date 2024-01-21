47e342750e0044423f916cb268cd26af5d065da5
frankRenlf <frank.renlingfeng@gmail.com>
2024-01-21 10:02:30 +0800

    update

---
+++ clrs/leetcode/T410.py
@@ -0,0 +1,29 @@
+# -*- coding: UTF-8 -*-
+"""
+    @Author : Frank.Ren
+    @Project : python_practice 
+    @Product : PyCharm
+    @createTime : 2024/1/21 09:56 
+    @Email : e1143935@u.nus.edu
+    @github : https://github.com/frankRenlf
+    @Description : hard
+"""
+from typing import List
+
+
+class Solution:
+    def splitArray(self, nums: List[int], m: int) -> int:
+        n = len(nums)
+        f = [[10 ** 18] * (m + 1) for _ in range(n + 1)]
+        sub = [0]
+        for elem in nums:
+            sub.append(sub[-1] + elem)
+
+        f[0][0] = 0
+        for i in range(1, n + 1):
+            for j in range(1, min(i, m) + 1):
+                for k in range(i):
+                    f[i][j] = min(f[i][j], max(f[k][j - 1], sub[i] - sub[k]))
+
+        return f[n][m]
+# if __name__ == "__main__":
