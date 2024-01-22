41183f570d1b7c95eeab0e0f67c13af870ac5eef
frankRenlf <frank.renlingfeng@gmail.com>
2024-01-22 09:43:42 +0800

    update

---
+++ clrs/leetcode/T670.py
@@ -0,0 +1,29 @@
+# -*- coding: UTF-8 -*-
+"""
+    @Author : Frank.Ren
+    @Project : python_practice 
+    @Product : PyCharm
+    @createTime : 2024/1/22 09:38 
+    @Email : e1143935@u.nus.edu
+    @github : https://github.com/frankRenlf
+    @Description : medium
+"""
+
+
+class Solution:
+    def maximumSwap(self, num: int) -> int:
+        s = list(str(num))
+        n = len(s)
+        maxIdx = n - 1
+        idx1 = idx2 = -1
+        for i in range(n - 1, -1, -1):
+            if s[i] > s[maxIdx]:
+                maxIdx = i
+            elif s[i] < s[maxIdx]:
+                idx1, idx2 = i, maxIdx
+        if idx1 < 0:
+            return num
+        s[idx1], s[idx2] = s[idx2], s[idx1]
+        return int(''.join(s))
+
+# if __name__ == "__main__":
