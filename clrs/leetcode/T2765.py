9bd392d05fe7d8773402ba0c1ee63cf300a9c9ab
frankRenlf <frank.renlingfeng@gmail.com>
2024-01-23 08:35:53 +0800

    update

---
+++ clrs/leetcode/T2765.py
@@ -0,0 +1,17 @@
+# -*- coding: UTF-8 -*-
+"""
+    @Author : Frank.Ren
+    @Project : python_practice 
+    @Product : PyCharm
+    @createTime : 2024/1/23 08:35 
+    @Email : e1143935@u.nus.edu
+    @github : https://github.com/frankRenlf
+    @Description : easy
+"""
+from typing import List
+
+
+class Solution:
+    def alternatingSubarray(self, nums: List[int]) -> int:
+
+# if __name__ == "__main__":
