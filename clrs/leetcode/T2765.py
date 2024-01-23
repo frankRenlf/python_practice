d6ae36890985414a573521a1612ec01a4404555e
frankRenlf <frank.renlingfeng@gmail.com>
2024-01-23 08:46:55 +0800

    update

--- clrs/leetcode/T2765.py
+++ clrs/leetcode/T2765.py
@@ -13,5 +13,18 @@ from typing import List
 
 class Solution:
     def alternatingSubarray(self, nums: List[int]) -> int:
+        res = -1
+        firstIndex = 0
+        for i in range(1, len(nums)):
+            length = i - firstIndex + 1
+            if nums[i] - nums[firstIndex] == (length - 1) % 2:
+                res = max(res, length)
+            else:
+                if nums[i] - nums[i - 1] == 1:
+                    firstIndex = i - 1
+                    res = max(res, 2)
+                else:
+                    firstIndex = i
+        return res
 
 # if __name__ == "__main__":
