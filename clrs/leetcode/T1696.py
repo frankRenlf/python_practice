05c6a04e870aaa66e3653ef3f2b18bb0e2e65092
Ren LIngfeng <e1143935@u.nus.edu>
2024-02-05 10:44:37 +0800

    update

--- clrs/leetcode/T1696.py
+++ clrs/leetcode/T1696.py
@@ -1,4 +1,32 @@
 from typing import List
+from collections import deque
+
+
 class Solution:
     def maxResult(self, nums: List[int], k: int) -> int:
-        
+        n = len(nums)
+        dp = [0] * n
+        dp[0] = nums[0]
+        for i in range(1, n):
+            dp[i] = nums[i] + max(dp[max(0, i - k) : i])
+        return dp[n - 1]
+
+
+class SolutionBest:
+    def maxResult(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        dp = [0] * n
+        dp[0] = nums[0]
+        queue = deque([0])
+        for i in range(1, n):
+            while queue and queue[0] < i - k:
+                queue.popleft()
+            dp[i] = dp[queue[0]] + nums[i]
+            while queue and dp[queue[-1]] <= dp[i]:
+                queue.pop()
+            queue.append(i)
+        return dp[n - 1]
+
+
+arr = [1, 2, 3]
+print(arr[1:2])
