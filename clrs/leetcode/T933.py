6ee52b83987002966ba0977def75d89c972126f6
Ren LIngfeng <e1143935@u.nus.edu>
2024-02-08 12:56:51 +0800

    update

--- clrs/leetcode/T933.py
+++ clrs/leetcode/T933.py
@@ -6,6 +6,38 @@
 #         self.right = right
 from typing import Optional
 from utils.TreeNode import TreeNode
+from collections import deque
+
+
 class Solution:
-    def isCousins(self, root: Optional[TreeNode], x: int, y: int) -> bool:
-        
+    def isCousins(self, root: TreeNode, x: int, y: int) -> bool:
+        # x 的信息
+        x_parent, x_depth, x_found = None, None, False
+        # y 的信息
+        y_parent, y_depth, y_found = None, None, False
+
+        # 用来判断是否遍历到 x 或 y 的辅助函数
+        def update(node: TreeNode, parent: TreeNode, depth: int):
+            if node.val == x:
+                nonlocal x_parent, x_depth, x_found
+                x_parent, x_depth, x_found = parent, depth, True
+            elif node.val == y:
+                nonlocal y_parent, y_depth, y_found
+                y_parent, y_depth, y_found = parent, depth, True
+
+        q = deque([(root, 0)])
+        update(root, None, 0)
+
+        while q:
+            node, depth = q.popleft()
+            if node.left:
+                q.append((node.left, depth + 1))
+                update(node.left, node, depth + 1)
+            if node.right:
+                q.append((node.right, depth + 1))
+                update(node.right, node, depth + 1)
+
+            if x_found and y_found:
+                break
+
+        return x_depth == y_depth and x_parent != y_parent
