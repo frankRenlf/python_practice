c282643f85cc3fd58d1ad64d0c412287e5243e55
Ren LIngfeng <e1143935@u.nus.edu>
2024-02-12 00:16:23 +0800

    update

---
+++ clrs/leetcode/T145.py
@@ -0,0 +1,19 @@
+from typing import Optional, List
+
+from clrs.leetcode.utils.TreeNode import TreeNode
+
+
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+
+        def postorder(node):
+            if node is None:
+                return
+            postorder(node.left)
+
+            postorder(node.right)
+            res.append(node.val)
+
+        postorder(root)
+        return res
