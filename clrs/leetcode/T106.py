62282039232928c9afbb6caa8d6bf1766608bae0
Ren LIngfeng <e1143935@u.nus.edu>
2024-02-21 10:31:54 +0800

    update

--- clrs/leetcode/T106.py
+++ clrs/leetcode/T106.py
@@ -14,18 +14,13 @@ class Solution:
             if postorder_left > postorder_right:
                 return None
 
-            # 前序遍历中的第一个节点就是根节点
             preorder_root = postorder_right
-            # 在中序遍历中定位根节点
             inorder_root = index[postorder[preorder_root]]
 
-            # 先把根节点建立出来
             root = TreeNode(postorder[preorder_root])
 
-            # 得到左子树中的节点数目
             size_right_subtree = inorder_right - inorder_root
-            # 递归地构造右子树，并连接到根节点
-            # 先序遍历中「从 左边界+1+左子树节点数目 开始到 右边界」的元素就对应了中序遍历中「从 根节点定位+1 到 右边界」的元素
+
             root.right = myBuildTree(
                 postorder_right - size_right_subtree,
                 postorder_right - 1,
@@ -33,8 +28,6 @@ class Solution:
                 inorder_right,
             )
 
-            # 递归地构造左子树，并连接到根节点
-            # 先序遍历中「从 左边界+1 开始的 size_left_subtree」个元素就对应了中序遍历中「从 左边界 开始到 根节点定位-1」的元素
             root.left = myBuildTree(
                 postorder_left,
                 postorder_right - size_right_subtree - 1,
