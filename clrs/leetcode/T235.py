269a304e92364dfb5219b1a9765cbfec66188fca
Ren LIngfeng <e1143935@u.nus.edu>
2024-02-25 19:51:15 +0800

    update

---
+++ clrs/leetcode/T235.py
@@ -0,0 +1,16 @@
+from clrs.leetcode.utils.nodes import TreeNode
+
+
+class Solution:
+    def lowestCommonAncestor(
+        self, root: TreeNode, p: TreeNode, q: TreeNode
+    ) -> TreeNode:
+        ancestor = root
+        while True:
+            if p.val < ancestor.val and q.val < ancestor.val:
+                ancestor = ancestor.left
+            elif p.val > ancestor.val and q.val > ancestor.val:
+                ancestor = ancestor.right
+            else:
+                break
+        return ancestor
