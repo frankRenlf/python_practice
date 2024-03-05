e0505fb7bfe37a893d6f80564ca748510a9ef79a
Ren LIngfeng <e1143935@u.nus.edu>
2024-03-05 10:52:28 +0800

    update

--- clrs/leetcode/T1976.py
+++ clrs/leetcode/T1976.py
@@ -29,6 +29,40 @@ class Solution:
         return ways[-1]
 
 
+class SolutionPre:
+    def __init__(self):
+        self.ans = 0
+        self.least = sys.maxsize
+
+    def countPaths(self, n: int, roads: List[List[int]]) -> int:
+        mp = {}
+        linked = {}
+        for road in roads:
+            if road[0] not in mp:
+                mp[road[0]] = []
+            mp[road[0]].append([road[1], road[2]])
+            if road[1] not in mp:
+                mp[road[1]] = []
+            mp[road[1]].append([road[0], road[2]])
+
+        def dfs(x, f, valsum):
+            if valsum > self.least:
+                return
+            if x == n - 1:
+                if valsum == self.least:
+                    self.ans += 1
+                elif valsum < self.least:
+                    self.ans = 1
+                    self.least = valsum
+                return
+            for way in mp[x]:
+                if way[0] != f:
+                    dfs(way[0], x, valsum + way[1])
+
+        dfs(0, -1, 0)
+        return self.ans
+
+
 if __name__ == "__main__":
     sol = Solution()
     print(
