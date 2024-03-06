98d4477f5dba4ca88ea3be8f8545c63be53c5473
Ren LIngfeng <e1143935@u.nus.edu>
2024-03-06 11:11:35 +0800

    update

---
+++ clrs/leetcode/T2917.py
@@ -0,0 +1,12 @@
+import math
+from typing import List
+
+
+class Solution:
+    def findKOr(self, nums: List[int], k: int) -> int:
+        ans = 0
+        for i in range(31):
+            cnt = sum(1 for num in nums if ((num >> i) & 1) > 0)
+            if cnt >= k:
+                ans |= 1 << i
+        return ans
