2ccc0f29fe699ee20e1d3ddb3af6c945af8b6919
Ren LIngfeng <e1143935@u.nus.edu>
2024-03-07 10:55:33 +0800

    update

--- clrs/leetcode/T2575.py
+++ clrs/leetcode/T2575.py
@@ -3,3 +3,16 @@ from typing import List
 
 class Solution:
     def divisibilityArray(self, word: str, m: int) -> List[int]:
+        n = len(word)
+        div = [0] * n
+        cur = 0
+        for i in range(0, n):
+            cur = (cur * 10 + int(word[i])) % m
+            if cur == 0:
+                div[i] = 1
+        return div
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.divisibilityArray(word="998244353", m=3))
