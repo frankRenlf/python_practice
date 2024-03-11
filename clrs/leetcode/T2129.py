73035f41c2e07ea6a591b1de8968d2469b3535f0
Ren LIngfeng <e1143935@u.nus.edu>
2024-03-11 10:30:39 +0800

    update

--- clrs/leetcode/T2129.py
+++ clrs/leetcode/T2129.py
@@ -1,3 +1,9 @@
 class Solution:
     def capitalizeTitle(self, title: str) -> str:
-        
+        subs = title.split(" ")
+        for i, sub in enumerate(subs):
+            if len(sub) > 2:
+                subs[i] = sub[0].upper() + sub[1:].lower()
+            else:
+                subs[i] = sub.lower()
+        return "".join(subs)
