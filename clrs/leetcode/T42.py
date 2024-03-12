11a912d309d369e0634d5c284a503b41e6119b02
Ren LIngfeng <e1143935@u.nus.edu>
2024-03-12 16:03:51 +0800

    update

--- clrs/leetcode/T42.py
+++ clrs/leetcode/T42.py
@@ -1,29 +1,18 @@
-# -*- coding: UTF-8 -*-
-"""
-    @Author : Frank.Ren
-    @Project : python_practice 
-    @Product : PyCharm
-    @createTime : 2023/7/23 09:29 
-    @Email : sc19lr@leeds.ac.uk
-    @github : https://github.com/frankRenlf
-    @Description : hard
-"""
 from typing import List
 
 
 class Solution:
     def trap(self, height: List[int]) -> int:
-        if not height:
-            return 0
-
         n = len(height)
-        leftMax = [height[0]] + [0] * (n - 1)
+        left = [0] * n
+        left[0] = height[0]
+        right = [0] * n
+        right[-1] = height[-1]
         for i in range(1, n):
-            leftMax[i] = max(leftMax[i - 1], height[i])
-
-        rightMax = [0] * (n - 1) + [height[n - 1]]
+            left[i] = max(left[i - 1], height[i])
         for i in range(n - 2, -1, -1):
-            rightMax[i] = max(rightMax[i + 1], height[i])
-
-        ans = sum(min(leftMax[i], rightMax[i]) - height[i] for i in range(n))
+            right[i] = max(right[i + 1], height[i])
+        ans = 0
+        for i in range(n):
+            ans += min(left[i], right[i]) - height[i]
         return ans
