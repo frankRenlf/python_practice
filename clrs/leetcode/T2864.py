7a0f7d61a9bbcb6a674954f2eabb33e1d9a8f95e
Ren LIngfeng <e1143935@u.nus.edu>
2024-03-13 10:28:03 +0800

    update

--- clrs/leetcode/T2864.py
+++ clrs/leetcode/T2864.py
@@ -1,3 +1,14 @@
+from collections import Counter
+
+
 class Solution:
     def maximumOddBinaryNumber(self, s: str) -> str:
-        
+        n = len(s)
+        mp = Counter(s)
+        mp["1"] -= 1
+        return "1" * mp["1"] + "0" * mp["0"] + "1"
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.maximumOddBinaryNumber("010"))
